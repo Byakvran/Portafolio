e5217fc5f5132f52cf47cc192a4acde21e4e88ce
Byakvran <65648485+Byakvran@users.noreply.github.com>
2020-07-01 07:31:54 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,25 +2,22 @@ $(document).ready(main);
 
 var contador = 1;
 
-function main (){
-	$('.menu_bar').click(function(){
-		if(contador == 1){
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});		
-		}
+function main() {
+  $(".menu_bar").click(function () {
+    if (contador == 1) {
+      $("nav").animate({
+        left: "0",
+      });
+      contador = 0;
+    } else {
+      contador = 1;
+      $("nav").animate({
+        left: "-100%",
+      });
+    }
+  });
 
-	});
-
-    $('.submenu').click(function(){
-	$(this).audio('.audio').slideToggle()
-});
-
+  $(".submenu").click(function () {
+    $(this).audio(".audio").slideToggle();
+  });
 }
-
